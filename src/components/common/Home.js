e0bc232766a5cb2d0a2c11307d9d85af6e054f55
Shawn-NG <nghengdi10@gmail.com>
2021-11-09 20:11:50 +0000

    set up the basic React front end app

---
+++ src/components/common/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import axios from 'axios'
+
+async function getAllUser() {
+  const { data } = await axios.get('http://127.0.0.1:8000/post/')
+  console.log(data)
+  return data
+}
+
+function Home() {
+  let [state, setState] = React.useState([])
+
+  async function APIcalltogetUser() {
+    try {
+      const data = await getAllUser()
+      setState([data])
+    } catch (err) {
+      console.error('There is error', err)
+    }
+  }
+
+  React.useEffect(() => {
+    APIcalltogetUser()
+  }, [])
+  console.log(state)
+  return <>{state.length !== 0 ? <p>all g</p> : <p>there is nothing</p>}</>
+}
+
+export default Home
