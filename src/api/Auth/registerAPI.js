66d157cae0b22dec268885457001be8e2588db2f
Shawn-NG <nghengdi10@gmail.com>
2021-11-12 20:10:05 +0000

    registering api call implementation

--- src/api/Auth/registerAPI.js
+++ src/api/Auth/registerAPI.js
@@ -3,5 +3,7 @@ import axios from 'axios'
 import baseURL from '../URL'
 
 function registering(data) {
-  return axios.post(`${baseURL}register/`, data)
+  return axios.post(`${baseURL}api/register`, data)
 }
+
+export default registering
